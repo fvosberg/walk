a5bd9375d50ec501f4bf23e5bc63be2bfbe91790
Frederik Vosberg <hello@frederikvosberg.de>
2017-09-24 11:05:06 +0200

    [TASK] refactor to a type

--- strings.go
+++ strings.go
@@ -7,30 +7,32 @@ import (
 )
 
 // Strings walks over all strings of the first parameter and executes the given function on it
-func Strings(i interface{}, fn func(string) string) error {
+func Strings(i interface{}, fn onStrings) error {
 	rv := reflect.ValueOf(i)
 	if rv.Kind() != reflect.Ptr && rv.Kind() != reflect.Slice {
 		return errors.New("Couldn't set the value - need pointer or slice as argument")
 	}
-	return strings(rv, fn)
+	return fn.walk(rv)
 }
 
-func strings(rv reflect.Value, fn func(string) string) error {
+type onStrings func(string) string
+
+func (fn onStrings) walk(rv reflect.Value) error {
 	switch rv.Kind() {
 	case reflect.String:
-		applyString(rv, fn)
+		fn.apply(rv)
 	case reflect.Ptr:
-		return strings(rv.Elem(), fn)
+		return fn.walk(rv.Elem())
 	case reflect.Slice:
 		for i := 0; i < rv.Len(); i++ {
 			iv := rv.Index(i)
 			if iv.Kind() == reflect.String {
-				applyString(iv, fn)
+				fn.apply(iv)
 			}
 		}
 	case reflect.Struct:
 		for i := 0; i < rv.NumField(); i++ {
-			err := strings(rv.Field(i), fn)
+			err := fn.walk(rv.Field(i))
 			if err != nil {
 				return errors.Wrapf(err, "processing field %#v failed", rv)
 			}
@@ -39,7 +41,7 @@ func strings(rv reflect.Value, fn func(string) string) error {
 	return nil
 }
 
-func applyString(rv reflect.Value, fn func(string) string) {
+func (fn onStrings) apply(rv reflect.Value) {
 	if !rv.CanSet() {
 		return
 	}
